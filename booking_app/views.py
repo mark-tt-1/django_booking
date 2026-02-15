b5ea23db72f51cc90b56e2dcf3fb6c8875ea314c
Mark TT <mtm282a@gmail.com>
2026-02-15 21:49:33 +0500

    add RoomViewSet and BookingViewSet API

--- booking_app/views.py
+++ booking_app/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+from .models import Room,Booking
+from .serializers import RoomSerializer,BookingSerializer
+
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+
+class RoomViewSet(ModelViewSet):
+    queryset=Room.objects.all()
+    serializer_class=RoomSerializer
+    permission_classes=[IsAuthenticatedOrReadOnly]   
+
+    @action(detail=True, methods=["get"])
+    def bookings(self, request, pk=None):
+        room = self.get_object()
+        bookings = room.bookings.all()
+        serializer = BookingSerializer(bookings, many=True)
+        return Response(serializer.data)
+    
+
+class BookingViewSet(ModelViewSet):
+    queryset=Booking.objects.all()
+    serializer_class=BookingSerializer
+    permission_classes=[IsAuthenticatedOrReadOnly]
+    
+    
+
+
+
+
